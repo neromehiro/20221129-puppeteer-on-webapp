1711e0d1bba39f38d978cb9b274ca051eed383df
horihiro <hori.kawa.shima.tool+github@gmail.com>
2022-11-29 19:45:17 +0900

    update app

--- app.js
+++ app.js
@@ -7,12 +7,20 @@ const puppeteer = require('puppeteer');
 
 const port = process.env.PORT || 8080;
 http.createServer(async function (req, res) {
+  const url = new URL(req.url, `http://${req.headers.host}`);
+  if (url.pathname === '/screenshot') {
+    res.writeHead(200, { 'Content-Type': 'image/png' })
+    const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
+    const page = await browser.newPage();
+    await page.goto(url.searchParams.url || 'https://www.microsoft.com/ja-jp/');
+    await new Promise(r => setTimeout(r, 10000));
+    res.end(await page.screenshot())
+    await browser.close();
+    return
+  }
+
   res.writeHead(200, { 'Content-Type': 'text/plain' })
-  const host = os.hostname()
-  const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
-  const page = await browser.newPage();
-  await page.goto('https://protoout.studio/');
-  await new Promise(r => setTimeout(r, 10000));
-  res.end(await page.screenshot())
-  await browser.close();
+  const host = os.hostname();
+  res.end(`I'm ${host}`);
+
 }).listen(port).on("listening", () => { console.log(`listening on http://localhost:${port}`) });
